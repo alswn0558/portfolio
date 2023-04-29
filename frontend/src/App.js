e3bf406cd2f5b7ab10bfb069a2aa396b7ac4c560
양민주 <mj@yangminjuui-MacBookAir.local>
2023-04-30 04:33:40 +0900

    info, 사진, radar 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,82 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from "react";
-import DonutChart from "./components/DonutChart";
+import RadarChart from "./components/RadarChart";
 import BarChart from "./components/BarChart";
 import styled from "styled-components";
 
 const ChartWrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: center;
   justify-content: center;
-  align-items: center;
-  height: 100vh;
+  align-items: flex-start;
+  height: 40vh;
 `;
 
 const ChartContainer = styled.div`
+  display: flex;  
   flex: 1;
-  width: 50%;
-  height: 100%;
-  padding: 20px;
+  width: 65%;
+  height: 95%;
+  padding: 1px;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
+  justify-content: center;
+  align-items: center;
 `;
 
 const Title = styled.h1`
   color: black;
   font-weight: bold;
   font-size: 3rem;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
   text-align: center;
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+const Information = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 20px;
+`;
+
+const Name = styled.h2`
+font-size: 2rem;
+margin-bottom: 10px;
+`;
+
+const School = styled.p`
+font-size: 1.2rem;
+margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+font-size: 1.2rem;
 `;
 
 const App = () => {
   return (
     <div>
-      <Title>포트폴리오</Title>
+      <Title>Portfolio</Title>
       <ChartWrapper>
         <ChartContainer>
-          <DonutChart />
+          <Information>
+            <Name>Minju Yang</Name>
+            <School>Kookmin University</School>
+            <Link href="https://github.com/alswn0558">GitHub</Link>
+            <Link href="https://www.linkedin.com/in/minju-yang-362614225/">LinkedIn</Link>
+          </Information>
+        </ChartContainer>
+        <ChartContainer>
+         <Image src="../mj.jpg" alt="Minju" />
+        </ChartContainer>
+      </ChartWrapper>
+      <ChartWrapper>
+        <ChartContainer>
+        <RadarChart />
         </ChartContainer>
         <ChartContainer>
           <BarChart />
---
+++ frontend/src/components/RadarChart.js
@@ -0,0 +1,52 @@
+import React, { useRef, useEffect } from 'react';
+import Chart from 'chart.js/auto';
+
+const RadarChart = (props) => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const myChartRef = chartRef.current.getContext("2d");
+    console.log(myChartRef)
+
+    const myChart = new Chart(myChartRef, {
+      type: "radar",
+      data: {
+        labels: ["인문 역량", "소통 역량", "글로벌 역량", "창의 역량", "전문 역량"],
+        datasets: [
+          {
+            label: "나의 핵심 역량",
+            data: [70.56, 63.92, 61.31, 55.03, 62.98],  // 이 부분 DB에서 가져와야 됨 
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "T점수 평균",
+            data: [50, 50, 50, 50, 50],
+            backgroundColor: "rgba(75, 137, 220, 0.2)",
+            borderColor: "rgba(75, 137, 220, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          r: {
+            angleLines: {
+                display: false
+            },
+            suggestedMin: 0,
+            suggestedMax: 80
+        }
+        },
+      },
+    });
+    return () => {
+      myChart.destroy(); // 이전 차트 파괴
+    };
+  }, []);
+
+  return <canvas ref={chartRef} />;
+};
+
+export default RadarChart;
